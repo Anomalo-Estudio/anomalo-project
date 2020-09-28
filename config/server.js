909f2d8683beb12c4aae40aa830ac0701415c864
seha13 <out_sherreraa@independencia.com.mx>
2020-09-28 14:14:21 -0500

    inicio de proyecto

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85b919e2ec2c949bac3b1949900c420d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4bc3368a-a331-4cb1-8418-b5871beac3a2'
+};
